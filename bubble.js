f385bc1b3332e18fd5b4f524f77dec06d04900ca
umarshk-mb <umarbvt4430@gmail.com>
2022-09-04 13:56:19 +0530

    first commit to bubbble game

---
+++ bubble.js
@@ -0,0 +1,13 @@
+function myFunction1() {document.getElementById("bubble_1").style.backgroundColor = "orange";}
+function myFunction2() {document.getElementById("bubble_2").style.backgroundColor = "black";}
+function myFunction3() {document.getElementById("bubble_3").style.backgroundColor = "skyblue";}
+function myFunction4() { document.getElementById("bubble_4").style.backgroundColor = "purple";}
+
+
+function myFunction5() {
+  document.getElementById("bubble_1").style.backgroundColor = "grey";
+  document.getElementById("bubble_2").style.backgroundColor = "blue";
+  document.getElementById("bubble_3").style.backgroundColor = "red";
+  document.getElementById("bubble_4").style.backgroundColor = "green";
+  document.getElementById("btn").style.backgroundColor = "lightgreen";
+}
