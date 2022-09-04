b73c3807166441c8166ab97aabf120636306bf5d
umarshk-mb <umarbvt4430@gmail.com>
2022-09-04 16:19:21 +0530

    Updated

--- bubble.js
+++ bubble.js
@@ -1,7 +1,27 @@
-function myFunction1() {document.getElementById("bubble_1").style.backgroundColor = "orange";}
-function myFunction2() {document.getElementById("bubble_2").style.backgroundColor = "black";}
-function myFunction3() {document.getElementById("bubble_3").style.backgroundColor = "skyblue";}
-function myFunction4() { document.getElementById("bubble_4").style.backgroundColor = "purple";}
+function myFunction1() {
+  document.getElementById("bubble_1").style.backgroundColor = "orange";
+  document.getElementById("arrow_1").style.color = "green";
+  document.getElementById("arrow_1").style.transition = "1s";
+  document.getElementById("arrow_1").style.marginLeft = "-7.5%";
+}
+function myFunction2() {
+  document.getElementById("bubble_2").style.backgroundColor = "black";
+  document.getElementById("arrow_2").style.color = "green";
+  document.getElementById("arrow_2").style.transition = "1s";
+  document.getElementById("arrow_2").style.marginLeft = "-7.5%";
+}
+function myFunction3() {
+  document.getElementById("bubble_3").style.backgroundColor = "skyblue";
+  document.getElementById("arrow_3").style.color = "green";
+  document.getElementById("arrow_3").style.transition = "1s";
+  document.getElementById("arrow_3").style.marginLeft = "-7.5%";
+}
+function myFunction4() { 
+  document.getElementById("arrow_4").style.color = "green";
+  document.getElementById("arrow_4").style.transition = "1s";
+  document.getElementById("arrow_4").style.marginLeft = "-7.5%";
+  document.getElementById("bubble_4").style.backgroundColor = "purple";
+}
 
 
 function myFunction5() {
@@ -10,4 +30,10 @@ function myFunction5() {
   document.getElementById("bubble_3").style.backgroundColor = "red";
   document.getElementById("bubble_4").style.backgroundColor = "green";
   document.getElementById("btn").style.backgroundColor = "lightgreen";
+  document.getElementById("arrow_1").style.marginLeft = "80%";
+  document.getElementById("arrow_2").style.marginLeft = "80%";
+  document.getElementById("arrow_3").style.marginLeft = "80%";
+  document.getElementById("arrow_4").style.marginLeft = "80%";
+
 }
+
